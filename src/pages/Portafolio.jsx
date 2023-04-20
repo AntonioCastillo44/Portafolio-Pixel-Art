703e3a176346d692c4f1e3e07c7acba8fc59210a
unknown <jossecass@gmail.com>
2023-04-20 10:44:44 -0600

    Se actualiza portafolio

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -3,21 +3,64 @@ import pokedex from "../img/1-removebg-preview.png";
 import crud from "../img/2-removebg-preview.png";
 import rick from "../img/3-removebg-preview.png";
 import weather from "../img/4-removebg-preview.png";
-import background from "../img/5-removebg-preview.png"
 import { useState } from "react";
 import "../styles/Portafolio.css";
 import "../styles/Nav.css";
 
 const Portafolio = ({ handleClosePortafolio }) => {
-  const [activeIndex, setActiveIndex] = useState(0)
-  
-  const handlePrevClick = () => {
-    setActiveIndex(activeIndex === 0 ? 3 : activeIndex - 1)
-  }
 
-  const handleNextClick = () => {
-    setActiveIndex(activeIndex === 3 ? 0 : activeIndex + 1)
-  }
+  const projects = [
+    {
+      id: 1,
+      title: 'Pokédex',
+      subtitle: 'React router y Redux',
+      description: 'Una de mis creaciones más recientes fue una pokedek en la que traté de mantenerme fiel al diseño clásico de las cartas Pokémon. Para garantizar la seguridad de la aplicación, implementé una ruta protegida utilizando React Router. Además, para mejorar la gestión del estado de la aplicación, decidí practicar la implementación de Redux.',
+      image: pokedex,
+      link: 'https://pokedex-jach.netlify.app/'
+    },
+    {
+      id: 2,
+      title: 'Administrador de usuarios',
+      subtitle: 'CRUD’s y métodos HTTP',
+      description: 'Desarrollé una aplicación completa utilizando CRUD (Create, Read, Update, Delete) y métodos HTTP. Utilicé React y Hook Form para crear formularios de entrada de datos controlados y manejar la validación de los mismos. Además, implementé renderizado condicional para mostrar diferentes vistas según las acciones que el usuario quisiera realizar, lo que resultó en una interfaz amigable e intuitiva para el usuario.',
+      image: crud,
+      link: 'https://users-management-jach.netlify.app/'
+    },
+    {
+      id: 3,
+      title: 'Wiki de Rick and Morty',
+      subtitle: 'Listas y Keys, inputs controlados, renderizado condicional',
+      description: 'Utilicé Listas y Keys para mostrar de manera ordenada las dimenciones, tambien implementé inputs controlados para permitir al usuario buscar dimensión. Con el renderizado condicional, mostré información de cada una de las dimensión solo cuando el usuario buscaba o actualizaba la pagina.',
+      image: rick,
+      link: 'https://rick-and-morty-search-jach.netlify.app/'
+    },
+    {
+      id: 4,
+      title: 'App del clima',
+      subtitle: 'Componentes, Props, useState y useEffect',
+      description: 'Este fue mi primer proyecto con el consumo API´s, tuve la oportunidad de aprender y utilizar de manera efectiva los hooks de React como el useEffect y useState. Además, para asegurarme de comprender correctamente la documentación, me esforcé por programar un diseño creativo y atractivo que destacara entre los proyectos de mis compañeros.',
+      image: weather,
+      link: 'https://weather-consultant-jach.netlify.app/'
+    }
+  ]
+
+  const [currentProject, setCurrentProject] = useState(0)
+  
+    const handleNextProject = () => {
+      if (currentProject < projects.length - 1) {
+        setCurrentProject(currentProject + 1);
+      } else {
+        setCurrentProject(0);
+      }
+    }
+  
+    const handlePrevProject = () => {
+      if (currentProject > 0) {
+        setCurrentProject(currentProject - 1);
+      } else {
+        setCurrentProject(projects.length - 1);
+      }
+    }
   
   return (
     <section className="container__portafolio">
@@ -32,18 +75,18 @@ const Portafolio = ({ handleClosePortafolio }) => {
 
         <div className="portafolio__container slider">
 
-          <div className={activeIndex === 0 ? "slide active" : "slide"}>
+          <div>
             <div className="portafolio__contet" alt="Imagen 1">
               <div className="portafolio__info">
-                <h1 className="portafolio__title">Pokédex</h1>
+                <h1 className="portafolio__title">{projects[currentProject].name}</h1>
                 <h2 className="portafolio__subtitle">
-                  React router y Redux
+                  {projects[currentProject].subtitle}
                 </h2>
                 <p className="portafolio__content">
-                  Una de mis creaciones más recientes fue una pokedek en la que traté de mantenerme fiel al diseño clásico de las cartas Pokémon. Para garantizar la seguridad de la aplicación, implementé una ruta protegida utilizando React Router. Además, para mejorar la gestión del estado de la aplicación, decidí practicar la implementación de Redux.
+                  {projects[currentProject].description}
                 </p>
 
-                <a href="https://pokedex-jach.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <a href={projects[currentProject].link} target="_blank" rel="noopener noreferrer">
                   <button className="portafolio__button nav__button">
                     Página
                   </button>
@@ -52,116 +95,25 @@ const Portafolio = ({ handleClosePortafolio }) => {
               </div>
 
               <div className="portafolio__cellPhone">
-                <img className="contacto__logo" src={pokedex} alt="" />
+                <img className="contacto__logo"   src={projects[currentProject].image} alt="Ejemplo del portafolio" />
               </div>
             </div>
-          </div>
-
-          <div className={activeIndex === 1 ? "slide active" : "slide"}>
-            <div className="portafolio__contet" alt="Imagen 2">
-              <div className="portafolio__info">
-                <h1 className="portafolio__title">Administrador de usuarios</h1>
-                <h2 className="portafolio-subtitle-crud">
-                  {"CRUD’s y métodos HTTP"}
-                </h2>
-                <p className="portafolio__content">
-                Desarrollé una aplicación completa utilizando CRUD (Create, Read, Update, Delete) y métodos HTTP. Utilicé React y Hook Form para crear formularios de entrada de datos controlados y manejar la validación de los mismos. Además, implementé renderizado condicional para mostrar diferentes vistas según las acciones que el usuario quisiera realizar, lo que resultó en una interfaz amigable e intuitiva para el usuario.
-                </p>
-              
-                <a href="https://users-management-jach.netlify.app/" target="_blank" rel="noopener noreferrer">
-                  <button className="portafolio__button nav__button">
-                    Página
-                  </button>
-                </a>
-              
-              </div>
-
-              <div className="portafolio__cellPhone">
-                <img className="contacto__logo" src={crud} alt="" />
-              </div>
-            </div>
-          </div>
-
-          <div className={activeIndex === 2 ? "slide active" : "slide"}>
-            <div className="portafolio__contet" alt="Imagen 3">
-              <div className="portafolio__info">
-                <h1 className="portafolio__title"> Wiki de Rick and Morty</h1>
-                <h2 className="portafolio-subtitle-wiki">
-                Listas y Keys, inputs controlados, renderizado condicional
-                </h2>
-                <p className="portafolio__content">
-                  Utilicé Listas y Keys para mostrar de manera ordenada las dimenciones, tambien implementé inputs controlados para permitir al 
-                  usuario buscar dimensión. Con el renderizado condicional, mostré información de cada una de las dimensión solo cuando el usuario buscaba o actualizaba la pagina.
-                </p>
-               
-                <button className="portafolio__button nav__button">
-                  Página
-                </button>
-               
-              </div>
-
-              <div className="portafolio__cellPhone">
-                <img className="contacto__logo" src={rick} alt="" />
-              </div>
-
-            </div>
-          </div>
-
-          <div className={activeIndex === 3 ? "slide active" : "slide"}>
-               <div className="portafolio__contet" alt="Imagen 4">
-
-            <div className="portafolio__info">
-              <h1 className="portafolio__title"> App del clima </h1>
-              <h2 className="portafolio-subtitle-clima">
-              Componentes, Props, useState y useEffect 
-              </h2>
-              <p className="portafolio__content">
-              Este fue mi primer proyecto con el consumo {'API´s,'} tuve la oportunidad de aprender y utilizar de manera efectiva los hooks de React como el useEffect y useState. Además, para asegurarme de comprender correctamente la documentación, me esforcé por programar un diseño creativo y atractivo que destacara entre los proyectos de mis compañeros.
-              </p>
-
-              <button className="portafolio__button nav__button">
-                Página
-              </button>
-            
-            </div>
-
-            <div className="portafolio__cellPhone">
-              <img className="contacto__logo" src={weather} alt="" />
-            </div>
-
-          </div>
-          </div>
-
-          <div className="portafolio__contet">
-
-            <div className="portafolio__info">
-              <h1 className="portafolio__title"></h1>
-              <h2 className="portafolio__subtitle">
-              </h2>
-              <p className="portafolio__content"> </p>
-            </div>
-
-            <div className="portafolio__cellPhone">
-              <img className="contacto__logo" src={background} alt="" />
-            </div>
-
           </div>
 
         </div>
       
-
         <div className="buttons">
           <div>
             <i
               class="bx bx-chevron-left close_icon"
-              onClick={handlePrevClick}
+              onClick={handlePrevProject}
             ></i>
           </div>
 
           <div>
             <i
               class="bx bx-chevron-right close_icon"
-              onClick={handleNextClick}
+              onClick={handleNextProject}
             ></i>
           </div>
         </div>
